5c4c16f715fe7e54c758cfe6c15c5cef067fdf4e
Steven Park <info@stevenpark.co.uk>
2018-07-31 21:14:18 +0100

    Create component

---
+++ components/BandstandCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Icon } from 'expo';
+
+import Colors from '../constants/Colors';
+
+export default class TabBarIcon extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
