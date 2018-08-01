ab95ea83a531585e1071b119e5568cf3a7dc6289
Steven Park <info@stevenpark.co.uk>
2018-08-01 01:14:05 +0100

    Colour variables

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,9 +1,15 @@
-const tintColor = '#2f95dc';
-
+// Brand Colours
 const brandGreen      = '#62d3a2';
 const brandYellow     = '#ffff00';
 const brandPurple     = '#7f47dd';
 const brandDarkPurple = '#662d91';
+
+const tone20 = '#662d91';
+const tone40 = '#ccc';
+const tone60 = '#888';
+const tone80 = '#662d91';
+
+const tintColor = brandGreen;
 
 export default {
   tintColor,
@@ -11,6 +17,10 @@ export default {
   brandYellow,
   brandPurple,
   brandDarkPurple,
+  tone20,
+  tone40,
+  tone60,
+  tone80,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
