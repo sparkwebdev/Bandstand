7e8295c4752d6f32a6ce7b83512c28eab7cb26fa
Steven Park <info@stevenpark.co.uk>
2018-08-05 16:46:35 +0100

    Tweak Bandstand Card props and output

--- components/BandstandCard.js
+++ components/BandstandCard.js
@@ -14,10 +14,10 @@ import { MonoTextBold } from "./StyledTextBold";
 export default class BandstandCard extends React.Component {
   render() {
     const item = this.props.item;
-    const hasVisited = this.props.hasVisited;
-    const hasAudio = this.props.hasAudio;
-    const hasLink = this.props.hasLink;
-    const hasDescription = this.props.hasLink;
+    const hasVisited = this.props.hasVisited || false;
+    const hasAudio = this.props.hasAudio || false;
+    const hasLink = this.props.hasLink || false;
+    const hasDescription = this.props.hasLink || false;
     return (
       <TouchableWithoutFeedback
           onPress={() =>
@@ -42,10 +42,6 @@ export default class BandstandCard extends React.Component {
               <MonoTextBold style={styles.title}>{item.title}</MonoTextBold>
               <MonoTextBold style={styles.subtitle}>{item.location}</MonoTextBold>
               <MonoTextBold style={styles.dates}>{item.dates}</MonoTextBold>
-              {/* <View style={styles.actions}>
-                  <MonoTextBold style={styles.link}>More Info</MonoTextBold>
-                  <MonoTextBold style={styles.link}>Link</MonoTextBold>
-              </View> */}
               {hasDescription ? (
                 <MonoText style={styles.description}>{item.description}</MonoText>
               ) : null}
