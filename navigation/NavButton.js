677be29f535489a05cbe56c747ebb26d81225730
Steven Park <info@stevenpark.co.uk>
2018-08-12 12:01:39 +0100

    New navigation (Action button)

--- navigation/NavButton.js
+++ navigation/NavButton.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import ActionButton from 'react-native-action-button';
 import BlurView from "../components/BlurView";
 import { withNavigation } from 'react-navigation';
+import Colours from '../constants/Colors';
 
 // import { MonoTextBold } from "./components/StyledTextBold";
 
@@ -11,53 +12,57 @@ class NavButton extends React.Component {
     constructor(props) {
       super(props);
     }
+
+    menuIcon(active) {
+      return active ? <Image style={styles.marker} source={require("../assets/images/icon_close.png")} /> : <Image style={styles.marker} source={require("../assets/images/icon_menu.png")} />;
+    }
+
     render() {
       return (
-        <ActionButton buttonColor="rgba(231,76,60,1)" bgColor="rgba(0,0,0,0.5)" offsetY={22} position="center" backdrop={<BlurView />}>
-          <ActionButton.Item buttonColor='transparent' title="Welcome" onPress={() => this.props.navigation.navigate('Welcome')}>
-            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt.png")} />
+        <ActionButton buttonColor={Colours.brandPurple} bgColor="rgba(0,0,0,0.5)" degrees={0} renderIcon={this.menuIcon} hideShadow={true} verticalOrientation="down" offsetY={40} position="right" spacing={20} backdrop={<BlurView />}>
+          <ActionButton.Item buttonColor="transparent" textStyle={styles.textStyle} textContainerStyle={styles.textContainerStyle} hideLabelShadow={true} spaceBetween={0} title="Bandstands" onPress={() => this.props.navigation.navigate('Bandstands')}>
+            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_marker.png")} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='transparent' title="Bandstands" onPress={() => this.props.navigation.navigate('Bandstands')}>
-            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt.png")} />
+          <ActionButton.Item buttonColor="transparent" textStyle={styles.textStyle} textContainerStyle={styles.textContainerStyle} hideLabelShadow={true} spaceBetween={0} title="Locations" onPress={() => this.props.navigation.navigate('Locations')}>
+            <Image style={styles.marker} source={require("../assets/images/icon_marker.png")} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='transparent' title="Locations" onPress={() => this.props.navigation.navigate('Locations')}>
-            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt.png")} />
+          <ActionButton.Item buttonColor="transparent" textStyle={styles.textStyle} textContainerStyle={styles.textContainerStyle} hideLabelShadow={true} spaceBetween={0} title="Playlist" onPress={() => this.props.navigation.navigate('Playlist')}>
+            <Image style={styles.marker} source={require("../assets/images/icon_playlist.png")} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='transparent' title="Bandstand" onPress={() =>
+          <ActionButton.Item buttonColor="transparent" textStyle={styles.textStyle} textContainerStyle={styles.textContainerStyle} hideLabelShadow={true} spaceBetween={0} title="Events" onPress={() => this.props.navigation.navigate('Events')}>
+            <Image style={styles.marker} source={require("../assets/images/icon_info.png")} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor="transparent" textStyle={styles.textStyle} textContainerStyle={styles.textContainerStyle} hideLabelShadow={true} spaceBetween={0} title="Bandstand" onPress={() =>
             this.props.navigation.navigate("Bandstand", {
               // itemId: item.id
               itemId: 1
               })
             }>
-            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt_3.png")} />
-            <Text style={styles.markerText}>1</Text>
+            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt.png")} />
+            {/* <MonoTextBold style={styles.markerText}>1</MonoTextBold> */}
           </ActionButton.Item>
+          {/* <ActionButton.Item buttonColor="transparent" title="Welcome" onPress={() => this.props.navigation.navigate('Welcome')}>
+            <Image style={styles.marker} source={require("../assets/images/icon_bandstand_alt.png")} />
+          </ActionButton.Item> */}
         </ActionButton>
       )
     }
 }
 
 const styles = StyleSheet.create({
-  // <View style={styles.gradient}></View>
-  // gradient: {
-  //   position: 'absolute',
-  //   backgroundColor: '#fff',
-  //   width: '100%',
-  //   height: 100,
-  // },
   marker: {
-    width: 48,
-    height: 48,
+    width: '100%',
+    height: '100%',
   },
-  markerText: {
-    width: 48,
-    height: 48,
+  textStyle: {
     color: '#fff',
-    position: 'absolute',
-    fontSize: 26,
-    lineHeight: 48,
-    alignSelf: 'center'
-  }
+    fontFamily: 'space-mono-bold'
+  },
+  textContainerStyle: {
+    borderRadius: 0,
+    borderWidth: 0,
+    backgroundColor: Colours.brandPurple,
+  },
 });
 
 export default withNavigation(NavButton);
