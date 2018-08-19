146cdb9b928ac08a150f089bf7e218d2513b54fd
Steven Park <info@stevenpark.co.uk>
2018-08-19 23:28:12 +0100

    New Events layout

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { ScrollView, View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
 
 import Colours from '../constants/Colors';
 import NavigationHelpers from "../helpers/NavigationHelpers";
 
+import Prompt from "../components/Prompt";
 import { MonoText } from "../components/StyledText";
 import { MonoTextBold } from "../components/StyledTextBold";
 import NavButton from "../navigation/NavButton";
@@ -14,55 +16,32 @@ export default class SettingsScreen extends React.Component {
     // title: 'Events'
   };
 
+  renderItem = props => (
+    <View
+      style={[styles.container,
+        {
+          width: props.width,
+          height: props.height
+        }
+      ]}
+    >
+      {props.image ? <Image style={[styles.image, {resizeMode: props.imageResizeMode,}]} source={props.image} /> : null }
+      {props.content ? <props.content /> : null}
+      
+    </View>
+  );
+
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
-          <MonoTextBold style={styles.title}>Events</MonoTextBold>
-          <View style={styles.paragraph}>
-            <MonoTextBold>Performances are scheduled for:</MonoTextBold>
-          </View>
-          <View style={styles.paragraph}>
-            <MonoTextBold style={styles.strong}>Sat 1st Sept, 3pm</MonoTextBold>
-            <MonoText>
-              site of Portobello Prom’s old bandstand (Community Garden),{"\n"}
-              John Street{"\n"}
-              Edinburgh EH15 2EB
-            </MonoText>
-          </View>
-          <View style={styles.paragraph}>
-            <MonoTextBold style={styles.strong}>Weds 5th Sept, 8pm</MonoTextBold>
-            <MonoText>
-              The Skylark, 243 Portobello High Street,{"\n"}
-              Edinburgh EH15 2AW{"\n"}
-              (livemix from Ross Whyte with recordings of the composition together
-              with collected archive sound and film to accompany Bandstand Project
-              exhibition)
-            </MonoText>
-          </View>
-          <View style={styles.paragraph}>
-            <MonoTextBold style={styles.strong}>Sat 8th Sept, 3pm</MonoTextBold>
-            <MonoText>
-              Princes Street Gardens,{"\n"}
-              Edinburgh EH2 2HG{"\n"}
-              (live performances from Ross Whyte & Portobello Community Choir)
-            </MonoText>
-          </View>
-          <View style={styles.paragraph}>
-            <MonoText>
-              You can also download a copy of the event newspaper from our
-              website:
-            </MonoText>
-            <MonoTextBold
-              style={styles.link}
-              onPress={() =>
-                NavigationHelpers.openWebPage("http://www.artwalkporty.co.uk")
-              }
-            >
-              artwalkporty.co.uk
-            </MonoTextBold>
-          </View>
-        </ScrollView>
+        <AppIntroSlider
+          slides={slides}
+          renderItem={this.renderItem}
+          dotColor={Colours.brandPurple}
+          activeDotColor={Colours.brandYellow}
+          hideNextButton
+          hideDoneButton
+        />
         <NavButton />
       </View>
     );
@@ -72,24 +51,135 @@ export default class SettingsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 25,
-    paddingRight: 15,
-    paddingBottom: 15,
-    paddingLeft: 15,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    justifyContent: "center"
   },
-  title: {
-    fontSize: 30,
-    marginBottom: 20
-  },
-  strong: {
-    fontSize: 20
-  },
-  link: {
-    color: Colours.brandPurple,
-    fontSize: 20
-  },
-  paragraph: {
-    marginBottom: 20
-  }
+    contentContainer: {
+      paddingLeft: 20,
+      paddingRight: 20,
+      alignSelf: "center",
+      alignContent: "center",
+      maxWidth: 420,
+    },
+    text: {
+      fontSize: 17,
+      lineHeight: 24,
+      textAlign: "center"
+    },
+    textSmall: {
+      fontSize: 15
+    },
+    textLarge: {
+      fontSize: 25,
+      lineHeight: 50,
+      textAlign: "center"
+    },
+    textLarger: {
+      fontSize: 35,
+      lineHeight: 45,
+    },
+    textLargest: {
+      fontSize: 55,
+      lineHeight: 65,
+    },
+    textBold: {
+      fontSize: 24,
+      color: Colours.brandGreen,
+    },
+    textLink: {
+      color: Colours.brandPurple
+    },
+    textYellow: {
+      color: Colours.brandYellow,
+      textAlign: "center"
+    },
+    image: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        width: '100%',
+        height: '100%',
+    },
 });
+
+const slides = [
+  {
+    key: '0',
+    image: require('../assets/images/screens/events-00.jpg'),
+    imageResizeMode: 'cover',
+    content: () => (
+      <View style={styles.contentContainer}>
+        <MonoText style={styles.text}>
+          {/* <MonoTextBold style={[styles.textYellow, styles.textLargest]}>
+          Performances{"\n"}
+          </MonoTextBold> */}
+          <MonoTextBold style={[styles.textYellow, styles.textLarger]}>
+          Performances{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}
+          </MonoTextBold>
+        </MonoText>
+      </View>
+    )
+  },
+  {
+    key: '2',
+    image: require('../assets/images/screens/events-01.jpg'),
+    content: () => (
+      <View style={styles.contentContainer}>
+        <MonoText style={styles.text}>
+          <MonoTextBold style={styles.textBold}>
+          Sat 1st Sept{"\n"}3pm</MonoTextBold>
+          <MonoTextBold>
+          {"\n"}{"\n"}Ross Whyte and Portobello Community Choir
+          perform the complete work ‘The Great Exhibition’
+          at Portobello Prom old bandstand space
+          (Community Garden, by John Street){"\n"}{"\n"}</MonoTextBold>
+        </MonoText>
+      </View>
+    )
+  },
+  {
+    key: '3',
+    image: require('../assets/images/screens/events-02.jpg'),
+    content: () => (
+      <View style={styles.contentContainer}>
+        <MonoText style={styles.text}>
+          <MonoTextBold style={styles.textBold}>
+          Weds 5th Sept{"\n"}8.30-10pm</MonoTextBold>
+          <MonoTextBold>
+          {"\n"}{"\n"}Ross Whyte performs a live mix of sound recordings together with archival material relating to the project
+          The Skylark, 243 Portobello High Street, Edinburgh EH15 2AW
+          (to accompany project exhibition){"\n"}</MonoTextBold>
+        </MonoText>
+      </View>
+    )
+  },
+  {
+    key: '4',
+    image: require('../assets/images/screens/events-03.jpg'),
+    content: () => (
+      <View style={styles.contentContainer}>
+        <MonoText style={styles.text}>
+          <MonoTextBold style={styles.textBold}>
+          {"\n"}{"\n"}Sat 8th Sept{"\n"}3pm</MonoTextBold>
+          <MonoTextBold>
+          {"\n"}{"\n"}Ross Whyte and Portobello Community Choir
+          perform the complete work ‘The Great Exhibition’
+          at Ross Theatre Bandstand, Princes Street Gardens, Edinburgh
+          {"\n"}{"\n"}{"\n"}</MonoTextBold>
+          <MonoText style={styles.textSmall}>
+            You can also download a copy of the event newspaper from our 
+            website:{"\n"}
+            <MonoTextBold
+              style={styles.textLink}
+              onPress={() =>
+              NavigationHelpers.openWebPage("http://www.artwalkporty.co.uk")
+              }
+            >artwalkporty.co.uk
+            </MonoTextBold>
+          </MonoText>
+        </MonoText>
+      </View>
+    )
+  },
+]
